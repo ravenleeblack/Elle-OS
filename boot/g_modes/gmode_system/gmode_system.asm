efb0807995af2eeff93aeaf1c07e0c3bc986c6ae
Robert Nelson <lordravenblack36@gmail.com>
2024-02-27 15:26:33 -0600

    Update gmode_system.asm

--- boot/g_modes/gmode_system/gmode_system.asm
+++ boot/g_modes/gmode_system/gmode_system.asm
@@ -8,7 +8,7 @@ _set_gmode_system:
     mov ax, 4F02h   ; VESA BIOS call to set graphics mode
     mov bx, 980h    ; Mode 100h corresponds to 1440x900x32bpp
     int 10h         ; Call BIOS interrupt 10h to set the mode
+    ret             ; Return
 
-
-    ; Return
-    ret
+;need to add in the proper labels need to set multiple video modes instead of hard coding it and need to add the proper label to get infomation from vesa 
+;so we can use that data in our core and c.
