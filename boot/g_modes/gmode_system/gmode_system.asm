8b07d77acc30a4906facda4d93dbc65e9063d94e
Robert Nelson <lordravenblack36@gmail.com>
2024-02-27 15:05:20 -0600

    Update gmode_system.asm

--- boot/g_modes/gmode_system/gmode_system.asm
+++ boot/g_modes/gmode_system/gmode_system.asm
@@ -1,1 +1,15 @@
 
+[BITS 16]
+section .text
+global _set_gmode_system
+global vbe_info
+global _get_vbe_info
+
+_set_gmode_system:
+    mov ax, 4F02h   ; VESA BIOS call to set graphics mode
+    mov bx, 980h    ; Mode 100h corresponds to 1024x768x8bpp
+    int 10h         ; Call BIOS interrupt 10h to set the mode
+
+
+    ; Return
+    ret
