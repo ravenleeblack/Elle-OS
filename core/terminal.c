bf089fbdf6f22859c361f9375571125762f6d449
Robert Nelson <lordravenblack36@gmail.com>
2024-02-27 21:16:16 -0600

    Create terminal.c

---
+++ core/terminal.c
@@ -0,0 +1,11 @@
+#include "terminal.h"
+
+
+struct terminal *create_terminal(struct sub_frame *parent, int x, int y, int width, int height)
+{
+	struct terminal *term_element = (struct terminal *)core_malloc(sizeof(struct terminal));
+
+	term_element->element = create_terminal_element(parent->element, x, y, width, height);
+
+	return term_element;
+}
