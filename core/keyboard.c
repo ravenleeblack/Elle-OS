384dcf4da1e679df3e9b9c0d31198b38da1c166a
Robert Nelson <lordravenblack36@gmail.com>
2024-02-27 21:10:06 -0600

    Create keyboard.c

---
+++ core/keyboard.c
@@ -0,0 +1,70 @@
+#include "keyboard.h"
+
+
+unsigned char keyboard_map[128] = {
+	0, 27, '1', '2', '3', '4', '5', '6', '7', '8',
+	'9', '0', '-', '=', '\0',
+	'\t',
+	'q', 'w', 'e', 'r',
+	't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\n',
+	0,
+	'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';',
+	'\'', '`', 0,
+	'\\', 'z', 'x', 'c', 'v', 'b', 'n',
+	'm', ',', '.', '/', 0,
+	'*',
+	0,
+	' ',
+	0,
+	0,
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0,
+	0,
+	0,
+	0,
+	0,
+	0,
+	'-',
+	0,
+	0,
+	0,
+	'+',
+	0,
+	0,
+	0,
+	0,
+	0,
+	0, 0, 0,
+	0,
+	0,
+	0 };
+
+void request_keyboard(int i, int intr_code)
+{
+	unsigned char scancode;
+
+	scancode = inb(0x60);
+
+	while (scancode & 1 && (scancode & 0x20) == 0)
+	{
+		den keycode = inb(0x60);
+		den scan_code = keycode & 0x7f;
+		den key_state = !(keycode & 0x80);
+
+		if (key_state)
+		{
+		   puts(keyboard_map[(unsigned char)scan_code]);
+		}
+
+		scancode = inb(0x64);
+	}
+
+	outb(0x20, 0x20);
+}
+
+
+/* Installs the keyboard handler into IRQ1 */
+void load_keyboard()
+{
+	load_request(req_keyboard, request_keyboard);
+}
