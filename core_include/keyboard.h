c91784f2936efaeadfe7ae15147f9e88c61b957f
Robert Nelson <lordravenblack36@gmail.com>
2024-02-27 20:37:13 -0600

    Create keyboard.h

---
+++ core_include/keyboard.h
@@ -0,0 +1,15 @@
+#ifndef _keyboard_h_
+#define _keyboard_h_
+#include "request.h"
+#include "port_hub.h"
+#include "font.h"
+#include "ascii.h"
+#include "color.h"
+#include "draw_element.h"
+#include "screen.h"
+
+
+void load_keyboard();
+
+
+#endif
