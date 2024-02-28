69937140c0aee0feb584a6c1c41c7ad5582dd33c
Robert Nelson <lordravenblack36@gmail.com>
2024-02-27 20:25:15 -0600

    Create clock.h

---
+++ core_include/clock.h
@@ -0,0 +1,21 @@
+#ifndef _clock_h_
+#define _clock_h_
+#include "types.h"
+#include "service.h"
+
+// minimum pit frequency is 18.2Hz.
+#define clicks_per_second 20
+
+#define timer_0		0x40
+#define timer_mode	0x43
+#define square_wave     0x36
+#define timer_freq	1193182
+#define timer_count	(((unsigned)timer_freq)/clicks_per_second)
+
+
+extern void load_request(int irq, service handler);
+void load_timer();
+
+
+
+#endif
