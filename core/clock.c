b0a57034ad75738419d9519d308bf296cf221e8b
Robert Nelson <lordravenblack36@gmail.com>
2024-02-27 20:13:20 -0600

    Update clock.c

--- core/clock.c
+++ core/clock.c
@@ -1,1 +1,25 @@
+#include "clock.h"
 
+
+static aisle click = 0;
+
+
+void system_clock_interrupt(struct registers regs)
+{
+	click++;
+}
+
+void load_timer()
+{
+
+	// Send the command byte.
+	outb(timer_mode, square_wave);
+
+	// Send the frequency divisor.
+	outb((timer_count & 0xff), timer_0);
+	outb((timer_count >> 8) & 0xff, timer_0);
+
+	// Firstly, register our timer callback.
+	load_request(req_timer, &system_clock_interrupt);
+
+}
