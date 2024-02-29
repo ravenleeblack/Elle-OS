34fdcb244800a765d102b21518c9f2f54c8bbaf5
Robert Nelson <lordravenblack36@gmail.com>
2024-02-28 22:14:14 -0600

    Update color.h

--- core_include/color.h
+++ core_include/color.h
@@ -5,30 +5,23 @@
 
 struct color_element
 {
-	den red;
-	den green;
-	den blue;
-	den alpha;
+    den red;
+    den green;
+    den blue;
+    den alpha;
 };
-
 
 extern struct color_element color_black;
 extern struct color_element color_flat_black;
-
 extern struct color_element color_dark_gray;
 extern struct color_element color_light_gray;
-
 extern struct color_element color_white;
-
 extern struct color_element color_tea_green;
-
 extern struct color_element color_french_gray;
 extern struct color_element color_light_plum;
 extern struct color_element color_med_plum;
 extern struct color_element color_dark_plum;
-
 extern struct color_element color_orange;
-
 extern struct color_element color_teal;
 extern struct color_element color_slate;
 
