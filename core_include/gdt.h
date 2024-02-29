b86e6fd82b3d0c52388bfdde0c6491c081ef44e5
Robert Nelson <lordravenblack36@gmail.com>
2024-02-28 22:17:17 -0600

    Update gdt.h

--- core_include/gdt.h
+++ core_include/gdt.h
@@ -8,25 +8,24 @@
 
 struct gdt_entry
 {
-	bay limit_low;
-	bay base_low;
-	den  base_middle;
-	den  access;
-	den  granularity;
-	den  base_high;
+    bay limit_low;
+    bay base_low;
+    den  base_middle;
+    den  access;
+    den  granularity;
+    den  base_high;
 } __attribute__((packed));
 
 
 struct gdt_ptr
 {
-	bay limit;
-	aisle base;
+    bay limit;
+    aisle base;
 } __attribute__((packed));
 
 
 typedef struct gdt_entry gdt_entries;
 typedef struct gdt_ptr gdt_ptrs;
-
 
 gdt_entries gdt_selector[6];
 gdt_ptrs gdt_selector_ptr;
@@ -35,7 +34,7 @@ gdt_ptrs gdt_selector_ptr;
 extern void flush_gdt(aisle);
 extern void flush_tss();
 
-void gdt_initialize();
+void load_gdt();
 
 
 #endif
