a938a2287957439c5501539cbce7d55ab70a6a58
Robert Nelson <lordravenblack36@gmail.com>
2024-02-28 21:53:54 -0600

    Update frame_bitmap.c

--- core/frame_bitmap.c
+++ core/frame_bitmap.c
@@ -14,7 +14,7 @@ struct bitmap_element *create_sub_frame_bitmap(int width, int height)
 	init_bitmap.width = width;
 	init_bitmap.height = height;
 	init_bitmap.color_format = rgb_format;
-	init_bitmap.video_buffer = video_memory; // Allocate memory for the video buffer
+	init_bitmap.video_buffer = video_memory; 
 	return &init_bitmap;
 }
 
@@ -23,7 +23,7 @@ struct bitmap_element *create_terminal_bitmap(int width, int height)
 	init_bitmap.width = width;
 	init_bitmap.height = height;
 	init_bitmap.color_format = rgb_format;
-	init_bitmap.video_buffer = video_memory; // Allocate memory for the video buffer
+	init_bitmap.video_buffer = video_memory; 
 	return &init_bitmap;
 }
 
