2348eaf26fd8867374137fc03dbc38888cb0123c
Robert Nelson <lordravenblack36@gmail.com>
2024-02-28 21:16:46 -0600

    Update tmode_system.asm

--- boot/tmode/tmode_system.asm
+++ boot/tmode/tmode_system.asm
@@ -1,21 +1,19 @@
-; tmode_system - This file handles all the text mode routines needed for the rootling bios.
-
 %include "clear_screen.inc"
 %include "move_cursor.inc"
 %include "write.inc"
 
 
 _set_tmode_system:
-    pusha               ; Push all general-purpose registers onto the stack
+    pusha                ; push all general-purpose registers onto the stack
 
-    mov ah, 00h          ; Set the video mode to text mode
-    mov al, 03h          ; VGA text mode 80x25 - 4000 characters
-    int 10h              ; Call BIOS video services
+    mov ah, 00h          ; set the video mode to text mode
+    mov al, 03h          ; vga text mode 80x50 - 4000 characters
+    int 10h              ; call bios video services
 
-	mov ax, 1112h    
+    mov ax, 1112h    
     mov bh, 0
     mov bl, 32
-    int 10h              ; Call BIOS video services
+    int 10h              ; call bios video services
 
-    popa                 ; Pop all general-purpose registers from the stack
-    ret                  ; Return from the subroutine
+    popa                 ; pop all general-purpose registers from the stack
+    ret                  ; return to osload
