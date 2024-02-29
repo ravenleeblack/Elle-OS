d84cb4e9c721ddea5f0689e942e9497e6726c881
Robert Nelson <lordravenblack36@gmail.com>
2024-02-28 22:13:23 -0600

    Update terminal.c

--- core/terminal.c
+++ core/terminal.c
@@ -3,9 +3,7 @@
 
 struct terminal *create_terminal(struct sub_frame *parent, int x, int y, int width, int height)
 {
-	struct terminal *term_element = (struct terminal *)core_malloc(sizeof(struct terminal));
-
-	term_element->element = create_terminal_element(parent->element, x, y, width, height);
-
-	return term_element;
+    struct terminal *term_element = (struct terminal *)core_malloc(sizeof(struct terminal));
+    term_element->element = create_terminal_element(parent->element, x, y, width, height);
+    return term_element;
 }
