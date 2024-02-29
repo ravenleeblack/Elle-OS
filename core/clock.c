71c3758051e8619996f5a25e9b1a808e7451b691
Robert Nelson <lordravenblack36@gmail.com>
2024-02-28 21:25:19 -0600

    Update clock.c

--- core/clock.c
+++ core/clock.c
@@ -10,8 +10,8 @@ void system_clock_interrupt(struct registers regs)
 void load_timer()
 {
     outb(timer_mode, square_wave);                          // Send the command byte.
-	outb((timer_count & 0xff), timer_0);              	    // Send the frequency divisor.
-	outb((timer_count >> 8) & 0xff, timer_0);
+    outb((timer_count & 0xff), timer_0);              	    // Send the frequency divisor.
+    outb((timer_count >> 8) & 0xff, timer_0);
 	
-	load_request(req_timer, &system_clock_interrupt);  	    // request an interrupt for the system timer
+    load_request(req_timer, &system_clock_interrupt);  	    // request an interrupt for the system timer
 }
