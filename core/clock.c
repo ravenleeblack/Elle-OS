83a201af00f83dd50f8de1fca6a9fe928a946f39
Robert Nelson <lordravenblack36@gmail.com>
2024-02-28 21:23:22 -0600

    Update clock.c

--- core/clock.c
+++ core/clock.c
@@ -1,25 +1,18 @@
 #include "clock.h"
 
-
 static aisle click = 0;
-
 
 void system_clock_interrupt(struct registers regs)
 {
-	click++;
+   click++;
 }
 
 void load_timer()
 {
+    outb(timer_mode, square_wave);                          // Send the command byte.
 
-	// Send the command byte.
-	outb(timer_mode, square_wave);
-
-	// Send the frequency divisor.
-	outb((timer_count & 0xff), timer_0);
+	outb((timer_count & 0xff), timer_0);              	    // Send the frequency divisor.
 	outb((timer_count >> 8) & 0xff, timer_0);
-
-	// Firstly, register our timer callback.
-	load_request(req_timer, &system_clock_interrupt);
-
+	
+	load_request(req_timer, &system_clock_interrupt);  	    // request an interrupt for the system timer
 }
