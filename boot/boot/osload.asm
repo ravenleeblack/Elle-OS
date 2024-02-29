4ab54a6194aefabfcbf36f4aef7d6f2aac797ec0
Robert Nelson <lordravenblack36@gmail.com>
2024-02-28 20:47:35 -0600

    Update osload.asm

--- boot/boot/osload.asm
+++ boot/boot/osload.asm
@@ -2,22 +2,21 @@
 section .osload
 global osload
 
-
 osload:  jmp switch_pmode
 
 %include "gmode.asm"
 %include "tmode_system.asm"
 
 switch_pmode:
-    call enable_gate
+    call enable_gate                 ; enable a20 gate for additional memory
     call _set_gmode_system           ; set vesa graphics mode so we can use it frame buffer
-    ;call _set_tmode_system	     ; set text mode
+    ;call _set_tmode_system	         ; set text mode. Left here so I can return to build a proper bootloader menu later
 
     cli
-    lgdt [selector_ptr]
+    lgdt [selector_ptr]              ; load the gdt table
 
     mov eax, cr0                     ; get the status word
-    or eax, 1                        ; turn on the P bit
+    or eax, 1                        ; turn on the protected mode bit
     mov cr0, eax                     ; store the status word
 
     jmp core_mode_code_seg:pmode     ; jmp to pmode
@@ -37,7 +36,7 @@ pmode:
     mov esp, ebp
 
     [EXTERN _core]
-    call _core                      ; call the core. Yea boy!! Os loaded up without a file system.
+    call _core                      ; call the core. Yea boy!! Os loaded up without relying on a file system.
 
 halt:                          
     cli
