0e3f5b1c222cd38343cde4e465475a6e20336cdf
Robert Nelson <lordravenblack36@gmail.com>
2024-02-28 22:03:26 -0600

    Update main_frame.c

--- core/main_frame.c
+++ core/main_frame.c
@@ -3,16 +3,16 @@
 
 struct main_frame *create_main_frame(struct main_frame *parent, int x, int y, int width, int height)
 {
-	struct main_frame *main_frame = (struct main_frame *)core_malloc(sizeof(struct main_frame)); // Allocate memory for the main frame
+    struct main_frame *main_frame = (struct main_frame *)core_malloc(sizeof(struct main_frame)); // Allocate memory for the main frame
 
-	if (parent == NULL)
-	{
-		main_frame->element = create_main_element(NULL, x, y, width, height);
+    if (parent == NULL)
+    {
+	main_frame->element = create_main_element(NULL, x, y, width, height);
 
-		return main_frame;
-	}
-	else
-	{
-		return NULL; // Main frame cannot be a child
-	}
+	return main_frame;
+    }
+    else
+    {
+	return NULL;               // Main frame cannot be a child
+    }
 }
