d62d1823da914fb6c4242d6a8d398aa81c7fd23c
Robert Nelson <lordravenblack36@gmail.com>
2024-02-28 21:32:14 -0600

    Update core.c

--- core/core.c
+++ core/core.c
@@ -7,32 +7,28 @@ int init_core()
 	struct main_frame *main_frame = create_main_frame(root, 0, 0, screen_width, screen_height);
 	fill_main(0, 870, 1440, 30, color_flat_black);                                                          //create the taskbar
 
-	
 	struct sub_frame *app = create_sub_frame(main_frame, 10, 10, 600, 700);                                 // Create the application window
-	draw_strand("Terminal", 500, 16, color_dark_plum, color_light_plum);
-
+	draw_strand("Terminal", 500, 16, color_dark_plum, color_light_plum);                                    // Create the appliction title
 
 	struct terminal *term_element = create_terminal(app, 12, 26, 596, 682);   	                            // Create terminal element within the application window
 
-
 	//draw_hex(0xABCD1234, 200, 200, color_flat_black, color_white);                                        //write hex works just commented out
 
+	core_free(main_frame);  	                                                                            // free the main frame
+	core_free(root);                                                                                        // free the main root
 
-	core_free(main_frame);  	                                                                            // Free memory
-	core_free(root);
-
-	return 1; // Success
+	return 1; 
 }
 
 
 int _core(int argc, const char *argv[])
 {
-	struct registers regs;
+	struct registers regs;                      // I dont suppose this is right. Only here because core_request and core_service require an argument.
 
-	gdt_initialize();
-	idt_initialize();
+	load_gdt();                          
+	load_idt();
 
-	int core_initialized = init_core();
+	int core_initialized = init_core();        // needs to be called before interrrupt handling otherwise its a black screen in bochs.
 
 	handle_core_request(regs);
 	handle_core_service(regs);
@@ -40,12 +36,11 @@ int _core(int argc, const char *argv[])
 	load_keyboard();
 	load_timer();
 
-	asm("sti");
+	asm("sti");                               // enable interrupts
 
-	for (;;)
+	for (;;)                                  // loop required 
 	{
 		asm("hlt");
 	}
-
 	return 0;
 }
