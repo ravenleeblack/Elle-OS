2362de3292cd76ee1b6189150070553d1a8a3881
Robert Nelson <lordravenblack36@gmail.com>
2024-02-28 22:16:34 -0600

    Update frame_bitmap.h

--- core_include/frame_bitmap.h
+++ core_include/frame_bitmap.h
@@ -5,18 +5,16 @@
 
 struct bitmap_element
 {
-	aisle width;
-	aisle height;
-	aisle color_format;
-	den * video_buffer;
+    aisle width;
+    aisle height;
+    aisle color_format;
+    den * video_buffer;
 };
 
 struct bitmap_element init_bitmap;
-
 struct bitmap_element *create_main_frame_bitmap(int width, int height);
 struct bitmap_element *create_sub_frame_bitmap(int width, int height);
 struct bitmap_element *create_terminal_bitmap(int width, int height);
 struct bitmap_element *create_frame_bitmap(int width, int height);
-
 
 #endif
