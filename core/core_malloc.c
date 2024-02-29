05c2680d312754d73cf7472a85fafb9735d9bf38
Robert Nelson <lordravenblack36@gmail.com>
2024-02-28 21:35:41 -0600

    Update core_malloc.c

--- core/core_malloc.c
+++ core/core_malloc.c
@@ -2,20 +2,20 @@
 
 void *core_malloc(size size)
 {
-	if (heap_index + size > heap_size)
-	{
-		return NULL; // Out of memory
-	}
+    if (heap_index + size > heap_size)
+    {
+         return NULL; // Out of memory
+    }
 
-	void *ptr = &heap[heap_index];
-	heap_index += size;
-	return ptr;
+    void *ptr = &heap[heap_index];
+    heap_index += size;
+    return ptr;
 }
 
 void core_free(void *ptr)
 {
-	if (ptr >= (void *)&heap[0] && ptr < (void *)&heap[heap_size])
-	{
-		heap_index = (char *)ptr - (char *)&heap[0];
-	}
+    if (ptr >= (void *)&heap[0] && ptr < (void *)&heap[heap_size])
+    {
+        heap_index = (char *)ptr - (char *)&heap[0];
+    }
 }
