caad31d48608443a409ef39d9023870e1a4fd3ba
Robert Nelson <lordravenblack36@gmail.com>
2024-02-28 20:44:03 -0600

    Update osboot.asm

--- boot/boot/osboot.asm
+++ boot/boot/osboot.asm
@@ -1,5 +1,3 @@
-;osboot.asm
-
 [BITS 16]
 extern osload
 
@@ -8,15 +6,15 @@ osboot:
      xor ax, ax                      ; Initialize seg regsisters to 0 since linker script will be placing bootloader at org 0x7c00             
      mov ds, ax
      mov es, ax
-	 mov fs, ax
+     mov fs, ax
      mov gs, ax
 
      mov ss, ax
      mov sp, 0x7c00                  ; Place stack just below bootloader at 0x0000:0x7c00
-	 sti
+     sti
 
-     call read_osload
+     call read_osload                ; read enough sectors
 
-     jmp 0:osload
+     jmp 0:osload                    ; jmp to second stage osload
 
 %include "read_osload.inc"
